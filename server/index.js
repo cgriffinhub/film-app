c7dffff496f07640e0bfcd0c0d07d4e72e756880
cgriffinhub <chuckgriffin1987@gmail.com>
2019-03-07 20:54:41 -0500

    asdf

--- server/index.js
+++ server/index.js
@@ -1,56 +1,30 @@
-require('dotenv').config()
-const express = require('express');
-const bodyParser = require('body-parser');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const exphbs = require('express-handlebars');
-const customAuthMiddleware = require('./middleware/custom-auth-middleware');
+var express = require("express");
 
-// controller imports
-const userController = require('./controllers/user-controller');
-const viewsController = require('./controllers/views-controller');
+// Sets up the Express App
+// =============================================================
+var app = express();
+var fs = require('fs');
 
-
-// set up the Express App
-const app = express();
-const PORT = process.env.PORT || 3000;
-
-// Express middleware that allows POSTing data
-app.use(bodyParser.urlencoded({ extended: true }));
+var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
-// use the cookie-parser to help with auth token,
-// it must come before the customAuthMiddleware
-app.use(cookieParser());
-app.use(customAuthMiddleware);
+var PORT = process.env.PORT || 8080;
 
-// serve up the public folder so we can request static
-// assets from our html document
-app.use(express.static("public"));
-
-// set up handlebars
-app.set('views', path.join(__dirname, '/views'));
-app.engine('handlebars', exphbs({
-  defaultLayout: 'main',
-  extname: '.handlebars',
-  layoutsDir: 'server/views/layouts'
-}));
-app.set('view engine', 'handlebars');
-
-// hook up our controllers
-app.use(userController);
-app.use(viewsController);
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 
-// Requiring our models for syncing
-const db = require('./models/index');
+// Static directory
+app.use(express.static("app/public"));
 
-// sync our sequelize models and then start server
-db.sequelize.sync().then(() => {
-  // inside our db sync callback, we start the server.
-  // this is our way of making sure the server is not listening
-  // to requests if we have not yet made a db connection
-  app.listen(PORT, () => {
-    console.log(`App listening on PORT ${PORT}`);
-  });
+// Routes
+// =============================================================
+require("./app/routing/apiRoutes.js")(app);
+require("./app/routing/htmlRoutes.js")(app);
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
 });
